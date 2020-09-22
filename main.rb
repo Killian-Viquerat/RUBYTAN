4f8ca4b9bdda4af3e7cf0e279afcbfe7557a1d43
Killian Viquerat <kil.viquerat@gmail.com>
2020-09-22 21:22:44 +0200

    change title and change place of balls calculation

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ require_relative './Classes/world'
 class Game < Gosu::Window
     def initialize
       super 600, 800
-      self.caption = "BBTAN"
+      self.caption = "RUBYTAN"
       @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
       @screen = Vector2.new(self.width,self.height)
       @character = Character.new(@screen)
@@ -20,6 +20,15 @@ class Game < Gosu::Window
       @character.move(Vector2.new(-5,0)) if Gosu.button_down?(Gosu::KB_LEFT) || Gosu.button_down?(Gosu::KB_A)
       @character.move(Vector2.new(5,0))  if Gosu.button_down?(Gosu::KB_RIGHT) || Gosu.button_down?(Gosu::KB_D)
       @character.shoot if Gosu.button_down?(Gosu::KB_UP)
+      @character.balls.each_with_index  do |ball,index|
+        if(@character.balls[index] != nil)
+          if(ball.destruction)
+            @character.balls[index] = nil
+          else
+            ball.move
+          end
+        end
+      end
     end
     
     def draw
@@ -27,12 +36,7 @@ class Game < Gosu::Window
       @world.draw
       @character.balls.each_with_index  do |ball,index|
         if(@character.balls[index] != nil)
-          ball.move
-          if(ball.destruction)
-            @character.balls[index] = nil
-          else
-            ball.draw
-          end
+          ball.draw
         end
       end
     end
